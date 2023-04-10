eb4776fab8a6d255f54dcb7f46097622e90d48dd
Kauã F. N. Rodrigues <77804949+rodrigueskaua@users.noreply.github.com>
2023-04-09 23:43:54 -0300

    container and video background hosted

--- js/functions.js
+++ js/functions.js
@@ -8,3 +8,31 @@ $('.navbar-nav a[href^="#"]').on('click', function(e) {
 
   }, 150);
 })
+
+const slideButtons = document.querySelectorAll(".nav-btn")
+const videos = document.querySelectorAll(".video-slide")
+const containers = document.querySelectorAll(".container-fluid")
+
+slider = function(manual){
+  slideButtons.forEach((button) => {
+    button.classList.remove("active")
+  })
+
+  videos.forEach((video) => {
+    video.classList.remove("active")
+  })
+
+  containers.forEach((container) => {
+    container.classList.remove("active")
+  })
+
+  slideButtons[manual].classList.add("active"); 
+  videos[manual].classList.add("active"); 
+  containers[manual].classList.add("active"); 
+
+}
+slideButtons.forEach((button, i) => {
+  button.addEventListener("click", () => {
+    slider(i);
+  })
+})
